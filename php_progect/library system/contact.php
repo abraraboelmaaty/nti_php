26820fa63559428107550cb5fdb7b1b9a31c37d7
abraraboelmaaty <abraraboelmaaty24@gmail.com>
2021-07-01 16:25:13 -0700

    project v 4

--- php_progect/library system/contact.php
+++ php_progect/library system/contact.php
@@ -1,12 +1,13 @@
 <?php 
-   require './operation/functions.php';
+//    require './operation/functions.php';
    require './operation/connection.php';
    include 'header.php';
+   include 'navbar.php';
 
    if($_SERVER['REQUEST_METHOD'] == "POST"){
 
     $email    = clean($_POST['email']);
-    $password = clean($_POST['password']);
+    
     $textMessage=clean($_POST['textMessage']);
     
     $errors = [];
@@ -24,22 +25,13 @@
      }
   
   
-     // Validate Password . 
-     if(empty($password)){
-         $errors['password'] = "Password Field Required";
-     }else{
-  
-         if(strlen($password) < 6){
-          $errors['password'] = "Password Must Be >= 6 "; 
-         }
-  
-     }
+     
 
       if(count($errors) == 0){
 
         // handling login
-        $password = sha1($password); 
-        $sql = "" ;
+       
+        $sql = "INSERT INTO `contact`( `email`, `message`) VALUES ('$email','$textMessage')" ;
 
         $op = mysqli_query($con,$sql);
 
@@ -64,13 +56,10 @@
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
     
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password"  name="password" class="form-control" id="exampleInputPassword1">
-  </div>
+  
 
   <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <label for="exampleFormControlTextarea1">message</label>
     <textarea class="form-control" name="textMessage" id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
   
